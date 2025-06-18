547012a865fb39fdb77a09683becf60349d6cfda
Juan Rodriguez <juanerudev@gmail.com>
2025-06-18 12:19:25 -0500

    Realicé todos los servicios del paciente y los consumí desde el router de pacientes, se agregaron algunas validaciones para poder crear y se manejaron algunos errores

--- routes/paciente.router.js
+++ routes/paciente.router.js
@@ -8,4 +8,45 @@ router.get('/', async (req, res) => {
     res.json(pacientes);
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const paciente = await pacienteService.findOne(id);
+        res.json(paciente);
+    } catch (err) {
+        res.status(404).json(err.message);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const body = req.body;
+        const nuevoPaciente = await pacienteService.create(body);
+        res.status(201).json(nuevoPaciente);
+    } catch (err) {
+        res.status(400).json(err.message);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const body = req.body;
+        const paciente = await pacienteService.update(id, body);
+        res.json(paciente);
+    } catch (err) {
+        res.status(404).json(err.message);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const rta = await pacienteService.delete(id);
+        res.json(rta);
+    } catch (err) {
+        res.status(404).json(err.message);
+    }
+});
+
 module.exports = router;
--- services/paciente.service.js
+++ services/paciente.service.js
@@ -3,3 +3,31 @@ const Paciente = require('../models/paciente.model');
 exports.find = async () => {
     return await Paciente.find();
 }
+
+exports.findOne = async (id) => {
+    const paciente = await Paciente.findById(id);
+    if (!paciente) throw new Error('El paciente no se encontró');
+    return paciente;
+}
+
+exports.create = async (data) => {
+    const { nombre, documento, fechaNacimiento } = data;
+
+    if (!nombre || !documento || !fechaNacimiento) throw new Error('Hay campos que deben ser especificados');
+
+    const nuevoPaciente = new Paciente(data);
+    await nuevoPaciente.save();
+    return nuevoPaciente;
+}
+
+exports.update = async (id, data) => {
+    const paciente = await Paciente.findByIdAndUpdate(id, data, { new: true });
+    if (!paciente) throw new Error('El paciente no se encontró');
+    return paciente;
+}
+
+exports.delete = async (id) => {
+    const paciente = await Paciente.findByIdAndDelete(id);
+    if (!paciente) throw new Error('El paciente no se encontró');
+    return { message: 'Paciente eliminado correctamente' };
+}
